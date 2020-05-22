058ffdf5000febd34fc1a8dd2a9a84423928ff43
github username <justinzhao265@gmail.com>
2020-05-22 15:41:53 -0400

    frontend deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://employment-tracker.surge.sh/'
+    origins 'https://employment-tracker.surge.sh'
 
     resource '*',
       headers: :any,
